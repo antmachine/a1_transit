99392d5898100d922d497c32cb42dc15ac3d1a44
antmachine <anthonysallows@gmail.com>
2014-05-07 12:06:37 -0700

    20140507184151 CreateMains: migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506192900) do
+ActiveRecord::Schema.define(version: 20140507184151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "mains", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
