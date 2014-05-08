76ebd149bb34b8487f4cdf0d24eb40bff849ceaa
antmachine <anthonysallows@gmail.com>
2014-05-07 19:33:07 -0700

    Displays list of Agencies from 511.org

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -4,7 +4,7 @@ class AgenciesController < ApplicationController
 
 
 	def index
-		@raw_xml = RestClient.get "http://services.my511.org/Transit2.0/GetAgencies.aspx?token=ENV['511_KEY']"
+		@raw_xml = RestClient.get("http://services.my511.org/Transit2.0/GetAgencies.aspx?token=#{ENV['511_KEY']}")
 		@results = Hash.from_xml(@raw_xml)
 	end
 end
